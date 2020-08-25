2b45c3f986f8408c0ccd6ff39952e3a21fdd9e09
Rabindra Mahato <robinmanohar@gmail.com>
2020-08-25 20:30:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let input = document.getElementById("temp").value;
+
+let el = document.getElementById("convert");
+let strUser = el.options[el.selectedIndex].value;
+
+
+//console.log(strUser);
+console.log(input);
+
+function convertTo() {
+    console.log(input);
+    
+}
+
